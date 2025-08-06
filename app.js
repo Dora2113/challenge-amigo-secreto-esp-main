a48d386bacb3359115674933e545303faceaf089
Dora Elvia <miley.89elvia@gmail.com>
2025-08-06 12:57:30 -0600

    Inserte texto al H2

--- app.js
+++ app.js
@@ -3,4 +3,25 @@
 // Aquí deberás desarrollar la lógica para resolver 
 // el problema.
 
+let campo = document.querySelector('button');
+let amigos = [];
 
+
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+function agregarAmigo(){
+    alert('Por favor, inserte un nombre.');
+    return;
+}
+
+function añadirNombreSorteado() {
+    let nombreSecreto = Math.random()*5;
+}
+
+asignarTextoElemento('h2','Añada 5 nombres de sus amigos');
